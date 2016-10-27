4943885a17d9ab8f7a57f85d0bd663b10b411eee
chenrygithub <colin.henry@gmail.com>
2016-10-27 12:38:13 +0100

    Update index.php
    Removed the rsync bit

--- index.php
+++ index.php
@@ -4,8 +4,4 @@ echo "Welcome to the best application in the world.";
 
 echo "<br>All running on openshift";
 
-echo "<br> rsync is awesome";
-echo "<br> rsync is awesome";
-echo "<br> rsync is awesome";
-echo "<br> rsync is awesome";
 
